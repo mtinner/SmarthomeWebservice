77e7021a30f499b53245b2d0e9e050f4096bd421
mtinner <gHpeb(460>
2015-02-21 22:28:12 +0100

    send integers

--- Smarthome/src/ch/smarthome/bll/manager/LampManager.java
+++ Smarthome/src/ch/smarthome/bll/manager/LampManager.java
@@ -1,17 +1,17 @@
 package ch.smarthome.bll.manager;
 
 import ch.smarthome.bll.entity.Lamp;
-import ch.smarthome.dal.LampSocket;
+import ch.smarthome.dal.ClientLampSocket;
 
 public class LampManager {
-	private LampSocket tc = new LampSocket();
+	private ClientLampSocket tc = new ClientLampSocket();
 
 	public LampManager() {
 		super();
 	}
 
 	public Lamp get(int id) {
-		return new Lamp(id, tc.startClient());
+		return new Lamp(id, tc.startClient(id));
 	}
 
 	public Lamp set(Lamp lamp) {
--- Smarthome/src/ch/smarthome/dal/ClientLampSocket.java
+++ Smarthome/src/ch/smarthome/dal/ClientLampSocket.java
@@ -1,9 +1,12 @@
 package ch.smarthome.dal;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -11,15 +14,21 @@ import org.glassfish.jersey.internal.util.PropertiesHelper;
 
 import ch.smarthome.helper.PropertyHelper;
 
-public class LampSocket {
-	public boolean startClient() {
+public class ClientLampSocket {
+	public boolean startClient(int id) {
 		boolean state = false;
+		//int id = 3;
 		PropertyHelper property = new PropertyHelper();
 		try {
 			String host;
 			int serverPort = 9750;
 			host = property.getProperty("ELITEBOOK");
 			Socket s = new Socket(host, serverPort);
+			
+			DataOutputStream bw = new DataOutputStream(s.getOutputStream());
+			bw.writeInt(id);
+			//bw.close();
+			
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					s.getInputStream()));
 			state = Boolean.parseBoolean(br.readLine());
